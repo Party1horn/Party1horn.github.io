792c150d0cec2189ae46af5a1155ad7f7a271228
Jakob <50112875+Party1horn@users.noreply.github.com>
2019-08-13 21:17:01 +0200

    changes

---
+++ paint.js
@@ -0,0 +1,65 @@
+window.onload = function (){
+    var canvas = document.getElementById("myCanvas");
+    var ctx = canvas.getContext("2d");
+    var painting = document.getElementById("content");
+    var paintStyle = getComputedStyle(painting);
+    canvas.width = parseInt(paintStyle.getPropertyValue("width"));
+    canvas.height = parseInt(paintStyle.getPropertyValue("height"));
+
+    let inputColor = document.getElementById("inputColor");
+    let inputSize = document.getElementById("inputSize");
+
+    inputColor.onchange = function(e) {
+        console.log("Test1");
+        ctx.strokeStyle = inputColor.value;
+    };
+
+    console.log(inputColor);
+
+    inputSize.onchange = function(e) {
+        console.log("Test2");
+        ctx.lineWidth = inputSize.value;
+    };
+
+    var mouse = {x: 0, y: 0};
+
+    canvas.addEventListener('mousemove', function(e) {
+        mouse.x = e.pageX - this.offsetLeft;
+        mouse.y = e.pageY - this.offsetTop;
+    }, false);
+
+    canvas.addEventListener('touchmove', (e)=>{
+        mouse.x = e.pageX - this.offsetLeft;
+        mouse.y = e.pageY - this.offsetTop;
+    });
+
+    ctx.lineWidth = 3;
+    ctx.lineJoin = 'round';
+    ctx.lineCap = 'round';
+    ctx.strokeStyle = '#FF0000';
+
+    canvas.addEventListener('mousedown', function(e) {
+        ctx.beginPath();
+        ctx.moveTo(mouse.x, mouse.y);
+        canvas.addEventListener('mousemove', onPaint, false);
+    }, false);
+
+    canvas.addEventListener('touchstart', (e)=>{
+        ctx.beginPath();
+        ctx.moveTo(mouse.x, mouse.y);
+        canvas.addEventListener('mousemove', onPaint, false);
+    });
+
+    canvas.addEventListener('mouseup', function() {
+        canvas.removeEventListener('mousemove', onPaint, false);
+    }, false);
+
+    canvas.addEventListener('touchend', (e)=>{
+        canvas.removeEventListener('mousemove', onPaint, false);
+    });
+
+    var onPaint = function() {
+        ctx.lineTo(mouse.x, mouse.y);
+        ctx.stroke();
+    };
+}
